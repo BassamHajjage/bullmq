1b27603dedafc76fae29141ec1f54b0de5f1ec57
Bassam Hajjage <85369725+BassamHajjage@users.noreply.github.com>
2021-10-04 22:18:46 -0400

    Revert "added getParentId"

--- src/classes/job.ts
+++ src/classes/job.ts
@@ -5,7 +5,6 @@ import {
   errorObject,
   isEmpty,
   getParentKey,
-  getParentId,
   lengthInUtf8Bytes,
   tryCatch,
 } from '../utils';
@@ -151,7 +150,6 @@ export class Job<T = any, R = any, N extends string = string> {
     this.opts.backoff = Backoffs.normalize(opts.backoff);
 
     this.parentKey = getParentKey(opts.parent);
-    this.parentId = getParentId(opts.parent);
 
     this.toKey = queue.toKey.bind(queue);
   }
--- src/utils.ts
+++ src/utils.ts
@@ -95,11 +95,6 @@ export function getParentKey(opts: { id: string; queue: string }): string {
     return `${opts.queue}:${opts.id}`;
   }
 }
-export function getParentId(opts: { id: string; queue: string }): string {
-  if (opts) {
-    return `${opts.id}`;
-  }
-}
 
 export function jobIdForGroup(
   jobOpts: JobsOptions,
